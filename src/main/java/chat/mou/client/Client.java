58c31cd7815de28916186d37394fdb3328b80159
Tobias <cchawps@gmail.com>
2020-04-06 16:45:28 +0200

    added async server prototypes

--- src/main/java/chat/mou/client/Client.java
+++ src/main/java/chat/mou/client/Client.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.CompletableFuture;
 
 public class Client {
     public static void main(String[] args) throws Exception {
@@ -16,12 +17,30 @@ public class Client {
         // Input message from user
         final var reader = new BufferedReader(new InputStreamReader(System.in));
 
+        new Thread(() -> {
+            try {
+                while (true) {
+                    final var inputMessage = input.readUTF();
+                    System.out.println(inputMessage);
+
+                    if (inputMessage.equals("-S")) {
+                        break;
+                    }
+                }
+            }
+            catch (Exception exc) {
+                exc.printStackTrace();
+            }
+        });
+
         while (true) {
             final var userMessage = reader.readLine();
             output.writeUTF(userMessage);
             output.flush();
 
-            System.out.println(input.readUTF());
+            CompletableFuture.runAsync(() -> {
+
+            });
 
             if (userMessage.equals("-S")) {
                 break;
